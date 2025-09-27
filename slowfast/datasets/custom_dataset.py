54036288a470a4d315e79c561e6612b448c39f33
Frannyboo <120744504+Frannyboo@users.noreply.github.com>
2025-09-27 20:53:05 +0100

    Update custom_dataset.py

--- slowfast/datasets/custom_dataset.py
+++ slowfast/datasets/custom_dataset.py
@@ -3,6 +3,7 @@ import torch
 import torch.utils.data
 import torchvision.io as io
 import torchvision.transforms as T
+from torchvision.io import read_video
 
 # from slowfast.datasets import utils as utils
 # from slowfast.datasets.build import DATASET_REGISTRY
