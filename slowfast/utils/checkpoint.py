53f2593f8f30e27089679d6133ae12ad2d3b6c98
Frannyboo <120744504+Frannyboo@users.noreply.github.com>
2025-10-06 19:37:23 +0100

    Update checkpoint.py

--- slowfast/utils/checkpoint.py
+++ slowfast/utils/checkpoint.py
@@ -26,7 +26,7 @@ def make_checkpoint_dir(path_to_job):
     Args:
         path_to_job (string): the path to the folder of the current job.
     """
-    checkpoint_dir = os.path.join(path_to_job, "checkpoints")
+    checkpoint_dir = path_to_job
     # Create the checkpoint dir from the master process
     if du.is_master_proc() and not pathmgr.exists(checkpoint_dir):
         try:
@@ -43,7 +43,7 @@ def get_checkpoint_dir(path_to_job):
     Args:
         path_to_job (string): the path to the folder of the current job.
     """
-    return os.path.join(path_to_job, "checkpoints")
+    return path_to_job
 
 
 def get_path_to_checkpoint(path_to_job, epoch, task=""):
